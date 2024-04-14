89671a9e5a7439b93216ea9a9cc44c8fa2f2ae9b
JulianoNode <julianonode@gmail.com>
2024-04-14 17:25:46 -0300

    Mudando de estado a imagem React *Conhecendo o React Hook useState *

--- jmr-image-front/src/app/galeria/page.tsx
+++ jmr-image-front/src/app/galeria/page.tsx
@@ -1,26 +1,42 @@
+'use client'
 import { Template, ImageCard } from "@/components";
+
+import { useState } from "react";
 
 
 export default function GaleriaPage(){
+
+    const image1 = "https://ggayane.github.io/css-experiments/cards/force_mage-character.webp"
+    const image2 = "https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg"
+
+    const nome2 = 'Heroi Vermelho'
+    const nome1 = 'Mago Azul '
+
+    const [codigoImage, setCodigoImage] = useState<number>(2);
+    const [urlImage, setUrlImage] = useState<String>(image2);
+    const [nomeImage, setNomeImage] = useState<String>(nome2);
+
+    function mudarImagem(){
+        if (codigoImage == 1) {
+            setCodigoImage(2);
+            setUrlImage(image2)
+            setNomeImage(nome2)
+        } else {
+            setCodigoImage(1);
+            setUrlImage(image1)
+            setNomeImage(nome1)
+        }
+
+    }
+
     return(
         <Template>
-            <section className="grid grid-cols-6 gap-8">
-                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001" src="https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg"/> 
-                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001"src="https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg"/>
-                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001"src="https://ggayane.github.io/css-experiments/cards/force_mage-character.webp"/>
-                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001" src="https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp"/> 
-            
-                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001" src="https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg"/> 
-                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001"src="https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg"/>
-                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001"src="https://ggayane.github.io/css-experiments/cards/force_mage-character.webp"/>
-                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001" src="https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp"/> 
-           
-                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001" src="https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg"/> 
-                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001"src="https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg"/>
-                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001"src="https://ggayane.github.io/css-experiments/cards/force_mage-character.webp"/>
-                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001" src="https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp"/> 
-           
-            </section>
+            <button className="bg-red-400" onClick={mudarImagem}>Click para mudar</button>
+            <section className="grid grid-cols-5 gap-8">
+                <ImageCard nome={nomeImage} tamanho="12MB" dataUpload="10/12/2001" src={urlImage} /> 
+                <ImageCard nome={nomeImage} tamanho="12MB" dataUpload="10/12/2001" src={urlImage} />
+                <ImageCard nome={nomeImage} tamanho="12MB" dataUpload="10/12/2001" src={urlImage} />             
+            </section>            
         </Template>
         
     )
