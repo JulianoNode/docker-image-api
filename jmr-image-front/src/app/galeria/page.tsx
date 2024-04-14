667decfe4d07a6df44a5d47bbce2956f7dd02ce8
JulianoNode <julianonode@gmail.com>
2024-04-14 13:57:27 -0300

    Componente de Image Card REACT

--- jmr-image-front/src/app/galeria/page.tsx
+++ jmr-image-front/src/app/galeria/page.tsx
@@ -4,15 +4,23 @@ import { Template, ImageCard } from "@/components";
 export default function GaleriaPage(){
     return(
         <Template>
-            <ImageCard /> 
-            <ImageCard />
-            <ImageCard />
-            <ImageCard />
-
-            <ImageCard /> 
-            <ImageCard />
-            <ImageCard />
-            <ImageCard />
+            <section className="grid grid-cols-6 gap-8">
+                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001" src="https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg"/> 
+                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001"src="https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg"/>
+                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001"src="https://ggayane.github.io/css-experiments/cards/force_mage-character.webp"/>
+                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001" src="https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp"/> 
+            
+                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001" src="https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg"/> 
+                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001"src="https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg"/>
+                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001"src="https://ggayane.github.io/css-experiments/cards/force_mage-character.webp"/>
+                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001" src="https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp"/> 
+           
+                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001" src="https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg"/> 
+                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001"src="https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg"/>
+                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001"src="https://ggayane.github.io/css-experiments/cards/force_mage-character.webp"/>
+                <ImageCard nome='Heroi' tamanho="12MB" dataUpload="10/12/2001" src="https://ggayane.github.io/css-experiments/cards/dark_rider-character.webp"/> 
+           
+            </section>
         </Template>
         
     )
--- jmr-image-front/src/components/ImageCard.tsx
+++ jmr-image-front/src/components/ImageCard.tsx
@@ -1,23 +1,23 @@
-export const ImageCard: React.FC = () => {
+interface ImageCardProps {
+    nome?: string;
+    tamanho?: string;
+    dataUpload?: string;
+    src?: string;
+    extension?: string;
+}
 
+export const ImageCard: React.FC<ImageCardProps> = ({ 
+    nome, dataUpload, src, tamanho, extension
+}: ImageCardProps) => {
 
     return (
-        <div className="card relative bg-white rounded-md shadow-md transition-tranform 
-                        ease-in duration-300 transform hover:shadow-lg hover:-translate-y-2">
-           <img src="https://ggayane.github.io/css-experiments/cards/dark_rider-cover.jpg" className="h-56 w-full object-cover rounded-t-md" alt="" />
-           <div className="card-body p-4">
-                <h5 className="text-xl font-semibold mb-2 text-gray-600">Nome da Imagem</h5>
-                <p className="text-gray-600">Tamanho</p>
-                <p className="text-gray-600">Data Upload</p>
-               
-            </div>
-
-            <img src="https://ggayane.github.io/css-experiments/cards/force_mage-cover.jpg" className="h-56 w-full object-cover rounded-t-md" alt="" />
-           <div className="card-body p-4">
-                <h5 className="text-xl font-semibold mb-2 text-gray-600">Nome da Imagem</h5>
-                <p className="text-gray-600">Tamanho</p>
-                <p className="text-gray-600">Data Upload</p>
-               
+        <div className="card relative bg-white rounded-md shadow-md transition-tranform ease-in duration-300 transform hover:shadow-lg hover:-translate-y-2">
+            <img src={src} className="h-56 w-full object-cover rounded-t-md" alt="" />
+            <div className="card-body p-4">
+                <h5 className="text-xl font-semibold mb-2 text-gray-600">{nome}</h5>
+                <p className="text-gray-600">{extension}</p>
+                <p className="text-gray-600">{tamanho}</p>
+                <p className="text-gray-600">{dataUpload}</p>
             </div>
         </div>
     )
