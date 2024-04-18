c268368806e36ecf0d45d93c12435d2ad791d678
JulianoNode <julianonode@gmail.com>
2024-04-18 19:23:55 -0300

    Componentizando o InputText React

--- jmr-image-front/src/app/galeria/page.tsx
+++ jmr-image-front/src/app/galeria/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Template, ImageCard, Button } from "@/components";
+import { Template, ImageCard, Button, InputText } from "@/components";
 import { Image } from "@/resources/image/image.resource";
 import { useImageService } from '@/resources/image/image.service';
 import { useState } from "react";
@@ -40,9 +40,8 @@ export default function GaleriaPage(){
         <Template loading={loading}>    
             <section className="flex flex-col items-center justify-center my-5">
                 <div className="flex space-x-4">
-                    <input type="text" 
-                        onChange={event => setQuery(event.target.value)}
-                        className="border px-3 py-2 rounded-lg text-gray-900" /> 
+                    <InputText onChange={event => setQuery(event.target.value)} 
+                        placeholder="Digite Nome ou Tag"/> 
                         <select onChange={event => setExtension(event.target.value)} 
                                 className="border px-4 py-2 rounded-lg text-gray-900">
                             <option value="">All formats</option>
